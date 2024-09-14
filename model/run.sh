9f75b458ef88336f81d36cbe01a70cfd4a42b7db
Jarek Miszczak <jarek@miszczak.eu>
2024-09-14 12:32:49 +0200

    Updated main script

--- model/run.sh
+++ model/run.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
+# get the name of the experiment from the script
 expName=${0/.sh/}
 expName=${expName/./}
 expName=${expName/\//}
+
+# set the file contain the model 
+modelFile=axelrod_model_diversity.nlogo
+
+# set the file with experiment definitions
+expFile=experiments.xml
+
+#make sure that the output directory exists
+mkdir -p data
+
+# print some information
 echo "[INFO] Running experiment: " $expName
 echo -n "[INFO] Start time: "
-date +"%H:%M"
-netlogo-headless.sh --threads 16 --model indoors-virus-propagation.nlogo --setup-file experiments.xml --table $expName.csv --experiment $expName
-echo -n "[INFO] End time:"
-date +"%H:%M"
+date +"%d/%m - %H:%M"
 
+# run NetLogo in the headless mode
+# use 16 threads by default
+# save results using table format
+netlogo-headless.sh --threads 20 \
+  --model $modelFile \
+  --setup-file $expFile \
+  --table data/$expName.csv \
+  --experiment $expName
+
+# print some information after finishing
+echo -n "[INFO] End time: "
+date +"%d/%m - %H:%M"
